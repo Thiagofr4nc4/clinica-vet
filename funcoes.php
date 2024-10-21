d18a8a6af191c35754fc51b8d88bfa8b4b016f6f
Thiagofr4nc4 <thiagofr4nc4@gmail.com>
2024-10-21 17:03:02 -0300

    Aqui eu entendi que era para todo o codigo ser feito numa única página, então mudei a estrutura

--- funcoes.php
+++ funcoes.php
@@ -4,16 +4,28 @@ include "conexao.php";
 
 $message = "";
 
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['nome']) && isset($_POST['contato'])){
-    $nome = $conn->real_escape_string($_POST['nome']);
-    $contato = $conn->real_escape_string($_POST['contato']);
-    $sql = "INSERT INTO owners (name, contact) VALUES ('$nome', '$contato')";
-    if($conn->query($sql)===TRUE){
-        $message = "Novo tutor inserido com sucesso";
-    } else {
-        $message =  "Erro: " .$sql . "<br>" .$conn->error;
-    }
+//ADICIONAR NOVO USUARIO
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['nome']) && isset($_POST['contato'])){
+        $nome = $conn->real_escape_string($_POST['nome']);
+        $contato = $conn->real_escape_string($_POST['contato']);
+        $sql = "INSERT INTO owners (name, contact) VALUES ('$nome', '$contato')";
+        if($conn->query($sql)===TRUE){
+            $message = "Novo tutor inserido com sucesso";
+        } else {
+            $message =  "Erro: " .$sql . "<br>" .$conn->error;
+        }
     
 }
+//DELETAR USUARIO
+    if (isset($_POST['delete']) && isset($_POST['id'])) {
+        $id = intval($_POST['delete']);
+        $sql = "DELETE FROM owners WHERE id=$id";
+        
+        if ($conn->query($sql) === TRUE) {
+            $message = "Usuário excluído com sucesso";
+        } else {
+            $message = "Erro: " . $sql . "<br>" . $conn->error;
+        }
+    }
 
 ?> 
